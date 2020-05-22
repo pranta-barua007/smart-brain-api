1484451514fb5684e911c7febc06edd51423994d
pranta-barua007 <prantabarua247@gmail.com>
2020-05-23 00:14:52 +0600

    updated port

--- server.js
+++ server.js
@@ -36,4 +36,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is runnin on port ${process.env.PORT}`)
+});
