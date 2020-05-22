50bf27d8b62e10c61c1b4013fa070190c0f668b7
pranta-barua007 <prantabarua247@gmail.com>
2020-05-23 00:18:23 +0600

    upadate

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
+app.get('/', (req, res) => { res.send('its working') });
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
