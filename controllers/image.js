53e78b65fb3c5db7c8c0854dbc07aca4a6fef0d6
pranta-barua007 <prantabarua247@gmail.com>
2020-05-22 23:10:47 +0600

    added files

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '9db4d04262664eccafcb526077beb820'
+});
+
+const handleApiCall = (req, res) => {
+    const { input } = req.body;
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0])
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
