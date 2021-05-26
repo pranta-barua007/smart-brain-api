83876c6524cf91abebd4f6b00516a30b7e525e60
pranta-barua007 <prantabarua247@gmail.com>
2021-05-26 14:52:14 +0600

    Dockerized, implemented session & auth

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
 
@@ -11,17 +11,22 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-
+const auth = require('./middlewares/authorization');
 
 const db = knex({
     client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    }
+    connection: process.env.DATABASE_URL || process.env.POSTGRES_URI,
 });
+
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//       rejectUnauthorized: true
+//     }
+//   }
+// });
 
 const app = express();
 
@@ -31,21 +36,25 @@ const corsOptions = {
 }
 
 app.use(helmet());
-app.use(cors(corsOptions));
+if (process.env.NODE_ENV !== 'production') {
+  const morgan = require('morgan');
+  app.use(morgan('combined'));
+  app.use(cors());
+}else {
+  app.use(cors(corsOptions));
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({  extended: true }));
 
 
 app.get('/', (req, res) => { res.send('its working') });
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
-
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
-
-app.put('/image', (req, res) => { image.handleImage(req, res, db) });
-
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+app.post('/signin', signin.signinAuthentication(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db)})
+app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res)})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
